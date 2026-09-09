d34cda7b6f638ffd26145f6bfefa959229f183fe
thaizebertelli <thaizepaulabertelli@gmail.com>
2026-09-09 20:01:53 -0300

    feat: adiciona dados dos produtos e formatador de moeda

---
+++ src/data/produtos.js
@@ -0,0 +1,27 @@
+import imgOsso from '../assets/img/osso.png';
+import imgRaposa from '../assets/img/raposa.png';
+import imgVara from '../assets/img/vara.png';
+
+export const produtosIniciais = [
+  {
+    id: 1,
+    nome: "Mordedor Osso",
+    preco: 29.90,
+    quantidade: 1,
+    imagem: imgOsso
+  },
+  {
+    id: 2,
+    nome: "Pelúcia Raposinha",
+    preco: 49.90,
+    quantidade: 2,
+    imagem: imgRaposa
+  },
+  {
+    id: 3,
+    nome: "Varinha com Penas",
+    preco: 19.90,
+    quantidade: 1,
+    imagem: imgVara
+  }
+];
---
+++ src/utils/formatador.js
@@ -0,0 +1,6 @@
+export const formatarMoeda = (valor) => {
+  return valor.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+};
